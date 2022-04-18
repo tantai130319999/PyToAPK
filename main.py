208ec662984fe0c511f0e9edb285637bb3583501
tantai130319999 <75244749+tantai130319999@users.noreply.github.com>
2022-04-18 13:07:56 +0700

    PyToAPK
    Tantai commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from jnius import autoclass
+from kivy.clock import Clock
+from android.runnable import run_on_ui_thread
+from kivy.uix.widget import Widget
+
+WebView = autoclass('android.webkit.WebView')
+WebViewClient = autoclass('android.webkit.WebViewClient')
+activity = autoclass('org.kivy.android.PythonActivity').mActivity
+motion = autoclass('android.view.MotionEvent')
+
+@run_on_ui_thread
+def create_webview(*args):
+	webview = WebView(activity)
+	webview.getSettings().setJavaScriptEnabled(True)
+	wvc = WebViewClient()
+	webview.setWebViewClient(wvc)
+	activity.setContentView(webview)
+	webview.loadUrl('https://www.muale.com.vn')
+	motion.obtain(0, 5, motion.ACTION_DOWN, 50, 50, 0)
+
+
+class Wv(Widget):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.__functionstable__ = {}
+		Clock.schedule_once(create_webview, 0)
+
+
+class ServiceApp(App):
+	def build(self):
+		return Wv()
+
+if __name__ == '__main__':
+    ServiceApp().run()
